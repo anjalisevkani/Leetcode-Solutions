debec9ef466b07af76a280d67ae1378b0288b23a
Anjali Sevkani <152064397+anjalisevkani@users.noreply.github.com>
2026-09-26 12:39:09 +0530

    Time: 2 ms (70.48%) | Memory: 45.1 MB (51.03%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,64 +1,13 @@
 class Solution {
-
-    int[][][] dp;
-
     public int maxProfit(int k, int[] prices) {
-        int n = prices.length;
-
-        dp = new int[n][k + 1][2];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= k; j++) {
-                dp[i][j][0] = -1;
-                dp[i][j][1] = -1;
+        int n=prices.length;
+        int[][][] dp =new int[n+1][2][k+1];
+        for(int i=n-1;i>=0;i--){
+            for(int cap=1;cap<=k;cap++){
+                dp[i][1][cap]=Math.max(-prices[i]+dp[i+1][0][cap],dp[i+1][1][cap]);
+                dp[i][0][cap]=Math.max(prices[i]+dp[i+1][1][cap-1],dp[i+1][0][cap]);
             }
         }
-
-        return solve(0, 0, 1, k, prices);
-    }
-
-    int solve(int day, int transactions, int buy,
-              int k, int[] prices) {
-
-        // End of array
-        if (day == prices.length) {
-            return 0;
-        }
-
-        // Maximum transactions completed
-        if (transactions == k && buy == 0) {
-            return 0;
-        }
-
-        if (dp[day][transactions][buy] != -1) {
-            return dp[day][transactions][buy];
-        }
-
-        int profit;
-
-        if (buy == 1) {
-
-            // Buy
-            int buying = -prices[day]
-                    + solve(day + 1, transactions, 0, k, prices);
-
-            // Don't buy
-            int skip = solve(day + 1, transactions, 1, k, prices);
-
-            profit = Math.max(buying, skip);
-
-        } else {
-
-            // Sell
-            int selling = prices[day]
-                    + solve(day + 1, transactions + 1, 1, k, prices);
-
-            // Don't sell
-            int skip = solve(day + 1, transactions, 0, k, prices);
-
-            profit = Math.max(selling, skip);
-        }
-
-        return dp[day][transactions][buy] = profit;
+        return dp[0][1][k];
     }
 }
