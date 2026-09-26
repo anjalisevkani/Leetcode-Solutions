397b805a73845a96a32fe8a308e67b503542952f
Anjali Sevkani <152064397+anjalisevkani@users.noreply.github.com>
2026-09-26 12:36:09 +0530

    Time: 51 ms (43.93%) | Memory: 117.9 MB (31.44%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int[][][] dp =new int[n+1][2][3];
+        for(int i=n-1;i>=0;i--){
+            for(int cap=1;cap<=2;cap++){
+                dp[i][1][cap]=Math.max(-prices[i]+dp[i+1][0][cap],dp[i+1][1][cap]);
+                dp[i][0][cap]=Math.max(prices[i]+dp[i+1][1][cap-1],dp[i+1][0][cap]);
+            }
+        }
+        return dp[0][1][2];
+    }
+}
